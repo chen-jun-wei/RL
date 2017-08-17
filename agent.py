runnable, but wrong value

agent.py
@@ -1,179 +1,347 @@
+from __future__ import print_function
 import numpy as np
-
-
+from utils import softmax, cdf
+import copy
 
 class agent:
 
-    def __init__(self, env, start, discount):
+    def __init__(self, env, start, epoch, discount):
 
         self.state = start
         self.discount = discount         
         self.env = env
+        self.epoch = epoch
         self.step = 0
-       
+        self.isBreak = False
         # transition probability
-        
+        #
         # h x w         x a            x s'
-        #[state] [choosed action] [reached state]
+        # 
+        # [state] [choosed action] [reached state]
         #
         # in this example, choosed action is same as next state
         
-        self.tprob = np.ones(env.grid[0], env.grid[1], len(env.dir), len(env.dir)) / len(env.dir)
+        self.tprob = np.ones([env.size[0], env.size[1], len(env.dir), len(env.dir)]) / len(env.dir)
         
-        self.policy = np.random.randint(0, len(self.env.dir), (env.grid[0], env.grid[1]))
         
-        self.value = np.random.
-        if not np.prod(env.size > np.array(start)):        
+        # randomize the policy
+
+        self.policy = np.random.randint(0, len(self.env.dir), (env.size[0], env.size[1]))
         
-            print '[!] Start Position is not in Environment'
+        self.q_value = np.zeros([env.size[0], env.size[1], len(env.dir)])
+        
+        self.value = np.zeros([env.size[0], env.size[1]])
+        if not int(np.prod(np.array(env.size) > np.array(start))):        
+        
+            print ('[!] Start Position is not in Environment')
+
+
+        self.initialize()
     
+    def initialize(self):
 
-    def _probability(self):
+        for un in self.env.unavailable:
 
-        return np.random.uniform()
+            self.q_value[un[0],un[1]] = self.env.grid[un[0], un[1]]
+            self.value[un[0], un[1]] = self.env.grid[un[0], un[1]]
+        for tm in self.env.terminate:
 
-    def _available(self):
+            self.q_value[tm[0], tm[1]] = self.env.grid[tm[0], tm[1]]
+            self.value[tm[0], tm[1]] = self.env.grid[tm[0], tm[1]]
+
+    def available(self):
         
-        available = []
+        available_state = []
         
-        for d in self.dir:
-
-            temp = self._move(self.state, d)
-
-            try:
-                _ = self.env[temp[0], temp[1]]
-
-                if not np.isnan(_):
-                    available.append(temp)
-            except:
-                pass
-
-        return available # availabel state, not available move
-
-
-    def _move(self, state, direction):
+        available_move = []
         
-        next_state = state
+        for d in self.env.dir:
 
+            temp = self.move(self.state, d)
+            
+            # pass if 'move' is attempt to move across the border
+            
+            if (temp[0] >= 0 and temp[0] < self.env.size[0]) and \
+                    (temp[1] >= 0 and temp[1] < self.env.size[1]):
+                
+                _ = self.env.grid[temp[0], temp[1]]
+
+                #if not np.isnan(_):
+                
+                available_state.append(temp)
+                    
+                available_move.append(d)
+
+        return available_state, available_move # availabel state, not available move
+
+
+    def move(self, state, direction):
+        
+        next_state = copy.copy(state)
+        
         if direction == 'W':
 
-            next_state += [0,-1]
+            next_state[1] += -1
 
         elif direction == 'E':
 
-            next_state += [0, 1]
+            next_state[1] += 1
 
         elif direction == 'N':
 
-            next_state += [1, 0]
+            next_state[0] += 1
 
         elif direction == 'S':
 
-            next_state += [-1,0]
+            next_state[0] += -1
 
         else:
 
-            print 'No Direction'
+            print ('No Direction')
 
             raise
-
+        
         return next_state
 
     
+    def action_index(self, action):
 
-        pass
-
-    def get_value(self, state):
+        """
         
-            
-        max_action = 0
+        action : a string specify which direction to go
 
-        for a in self.available(): # iterate along available action
-            
-            value = 0
+        return : the index of direction
 
-            for next_s in self._available(): # each action change reach each state
+        """
 
-                value +=  transition(state, next_s) * (Reward + Value(-1)))
-            
-                                 
-
+        return self.env.dir.index(action)
     
+    def random_walk(self, epoch, show = False):
 
-        self.grid.set(state, max(max_val, d) for d in self.dir)
-    
-    def random_walk(self, total_epoch):
+        """
         
-        for i in xrange(total_epoch):
+        Learning Process
 
-            self.random_start()
+        """
 
-            if self.state in self.grid.terminate:
+        
+        
+        for i in xrange(epoch):
+
+            self.state = self.random_start()
+            
+            self.step = 0
+
+            print (self.state)
+                
+            #print self.isBreak
+            
+            #print i
+
+            if self.state in self.env.terminate or self.state in self.env.unavailable:
 
                 continue
+            
+            while not self.isBreak:
+                
+                print ('-------------------------------------------------------')
+                
+                print ('Epoch : {}, Step : {}, State : {}'.format(i+1, self.step, self.state))
+                
+                r = self.act(self.policy[self.state[0], self.state[1]])
 
-            while True:
+                self.step += 1
+                
+                if show:
 
-                r = self.act()
+                    self.show()
+
+            self.isBreak = False
 
     def random_start(self):
+
+        """
+        
+        randomly select a start position for each epoch
+
+        
+        """
         
         while True:
 
-            coord = [np.random.choice(self.size[0]), \
-                    np.random.choice(self.size[1])]
-            
-            if coord in self.grid.unavailable:
+            coord = [np.random.choice(self.env.size[0]), \
+                    np.random.choice(self.env.size[1])]
+                    
+            if coord in self.env.unavailable or coord in self.env.terminate:
 
                 continue
 
             break
 
-        return coord # set start
+        return coord # set start position
 
+    def isReachable(self, next_state):
 
+        return False if np.isnan(self.env.grid[next_state[0], next_state[1]]) else True
+                
     def act(self, action):
+
+        """
+
+        take the action and reach next state,
+        but the transition is stochastic,
+        which might not go the state one expected
+        
+        action : int -> action
+        """
         
         # random select action
-        #action = self.available() 
+        # action = self.available() 
 
         # h x w x a x s'
     
         # stochastic transition probability
         
-        tprob = softmax(self.policy[self.state[0],\
-                self.state[1], a])
+        tprob_available = []
+
+        for ava in self.available()[1]:
+        
+            tprob_available.append(self.tprob[self.state[0], self.state[1], \
+                    action, self.action_index(ava)])
+
+        tprob_available = np.array(tprob_available)
+
+        tprob_available = softmax(tprob_available, axis = 0)
         
         # available state [index]
+        
+        # reach next state sucessful
+    
+        #print 'available : ', self.available()[1]
+        
+        next_state = self.move(self.state, self.available()[1][self.transition(tprob_available)])
+        
+        print ('Next State : ', self.env.grid[next_state[0], next_state[1]])
 
-        self.state = self.avaibale()[self.transition(tprob, np.random.uniform())]
+        self.state = next_state if self.isReachable(next_state) else self.state
          
+        print ('Reachable : ', self.isReachable(next_state))
+
+        # update q value
+
+        # pns : probable next state
+        
+        if self.state in self.env.terminate:
+
+            # reset reward
+
+            self.reward = 0
+                
+            self.isBreak = True
+
+        elif self.isReachable(next_state) :
+            
+            maximum = 0
+            
+            for a in xrange(len(self.env.dir)):
+                #print ('possible next state value : ', self.value[pns[0],pns[1]])
+        
+                for pns, pnd in zip(self.available()[0], self.available()[1]):
+                    
+                    if pns in self.env.unavailable:
+
+                        continue
+                    
+                    #self.q_value[self.state[0], self.state[1], a] = self.tprob[self.state[0], self.state[1], a, self.action_index(pnd)] * \
+                     #       (self.reward_function() + self.discount * self.value[pns[0], pns[1]])
+                    maximum = max(maximum, self.tprob[self.state[0], self.state[1], a, self.action_index(pnd)] * (self.reward_function() + self.discount * self.value[pns[0], pns[1]])
+                # update value
+                self.q_value[self.state[0], self.state[1], a] = maximum
+
+                self.value[self.state[0], self.state[1]] = self.value_function()
+
+                if self.value[pns[0],pns[1]] > 50:
+                    self.show() 
+                    #raise
     # transition probability range, random probability
 
-    def transition(self, tprob, rprob):
+    def transition(self, tprob):
 
-        tprob = cdf(tprob)
+        """
+        
+        transition function
+        
+            randomly sample a number from uniform distribution
+
+
+        """
+
+        tprob_range = cdf(tprob)
+
+        rprob = np.random.uniform()
 
         for i in xrange(len(tprob)):
             
-            if tprob[i] < rprob and rprob < tprob[i+1]:
-
+            if tprob_range[i] < rprob and rprob < tprob_range[i+1]:
+                
                 return i # return which action to run
 
-    def reward_function(self, t, discount):
-
-        return self.reward += discount ** t * self.env.grid[self.state[0], self.state[1]
-
-    def value_function(self, state):
+    def reward_function(self):
         
-        temp = 0
+        return -0.1
+        #return self.discount ** self.step * self.value_function()
 
-        for i in len(self.env.dir):
+    def value_function(self, state = None, index = False):
         
-            self.[state[0], state[1], i]
-         
-        return
+        maximum = 0
+        
+        if state == None:
+
+            state = self.state
+
+        if index:
+        
+            return np.max(self.q_value[state[0], state[1]]), np.argmax(self.q_value[state[0], state[1]])
+        
+        else:
+            
+            return np.max(self.q_value[state[0], state[1]])
 
     def q_function(self, state, action):
 
+        return q_value[state[0], state[1], action]
 
+    def show(self):
+        
+        from utils import print2
+
+        for h in reversed(xrange(self.env.size[0])):
+
+            for w in xrange(self.env.size[1]):
+
+                #print2(self.value_function([h,w]))
+                
+                print('%3.3f' % (self.value[h,w] ), end = '')
+                print2(' | ')
+            
+            print (' ')
+
+
+
+        for h in reversed(xrange(self.env.size[0])):
+
+            for w in xrange(self.env.size[1]):
+
+                #print2(self.value_function([h,w]))
+                
+                if [h,w] == self.state:
+
+                    print2(' o ')
+
+                else:
+                    
+                    print2('   ')
+
+                print2(' | ')
+            
+            print (' ')

utils.py
@@ -1,6 +1,5 @@
 import numpy as np
-
-
+import sys
 
 def softmax(neurons, axis):
     
@@ -12,17 +11,23 @@ def softmax(neurons, axis):
 
 
 def cdf(probability):
+    
+#    print 'original : ', probability
 
-    for i in xrange(len(probability)):
+    for i in xrange(len(probability) + 1):
 
         if i == 0:
             
-            np.insert(probability, 0, 0)
+            probability = np.insert(probability, 0, 0)
 
             continue
-
+        
         probability[i] += probability[i-1]
-
+        
+     #   print '{} : {}'.format(i, probability)
     return probability
 
 
+def print2(to):
+
+    sys.stdout.write(str(to))